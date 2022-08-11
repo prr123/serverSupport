628ac97f1999eee56d9d3adfe5e95f86382b79de
prr123 <priemen@gmail.com>
2022-08-11 16:50:56 +0200

    add parse footer.yaml

--- creFooter.go
+++ creFooter.go
@@ -12,36 +12,74 @@ package main
 import (
 	"fmt"
 	"os"
-//    "gopkg.in/yaml.v3"
+    "gopkg.in/yaml.v3"
 //    util "util"
 )
 
 type footerYamlObj struct {
+	Domain string `yaml:"domain"`
+	HtmlDir string `yaml:"htmlDir"`
 	// Service
-	aboutUs string `yaml:"aboutUs"`
-	contact string `yaml:"contact"`
+	AboutUs string `yaml:"aboutUs"`
+	Contact string `yaml:"contact"`
 	// legal
 	// privacy policy
 	// data protection
-	dataProtect string `yaml:"dataProtect"`
+	DataProtect string `yaml:"dataProtect"`
 	// cookies
-	cookies string `yaml:"cookies"`
+	Cookies string `yaml:"cookies"`
 	// social media
-	fbook string `yaml:"facebook"`
-	twitter string `yaml:"twitter"`
-	linkedin string	`yaml:"linkedin"`
-	whatsapp string `yaml:"whatsapp"`
-
+//	fbook string `yaml:"facebook"`
+//	twitter string `yaml:"twitter"`
+//	linkedin string	`yaml:"linkedin"`
+//	whatsapp string `yaml:"whatsapp"`
 }
+
+func dispFooterYamlObj(footer *footerYamlObj) {
+
+	fmt.Println("*** Yaml Footer Obj ***")
+	fmt.Printf("  domain:    %s\n", footer.Domain)
+	fmt.Printf("  htmlDir:   %s\n", footer.HtmlDir)
+	fmt.Printf("  about us:  %s\n", footer.AboutUs)
+	fmt.Printf("  contact:   %s\n", footer.Contact)
+	fmt.Printf("  data prot: %s\n", footer.DataProtect)
+	fmt.Printf("  cookies:   %s\n", footer.Cookies)
+
+	return
+}
+
+func readYamlFil(yamlFil *os.File) (yamlftObj *footerYamlObj, err error) {
+
+	yamlFilInfo, err := yamlFil.Stat()
+    size := int(yamlFilInfo.Size())
+    yamlBuf := make([]byte,size)
+
+    _, err = yamlFil.Read(yamlBuf)
+    if err != nil {
+        return nil, fmt.Errorf("error reading yaml data: %v\n", err)
+    }
+
+//  fmt.Printf("yaml data:\n %s\n", string(yamlBuf))
+
+    yamlftObj = new(footerYamlObj)
+    err = yaml.Unmarshal(yamlBuf, yamlftObj)
+    if err != nil {
+        return nil, fmt.Errorf("unmarshall error: %v\n", err)
+    }
+
+//	fmt.Printf("yaml:\n%v\n", yamlftObj)
+	return yamlftObj, nil
+}
+
 
 func main () {
 
-	infil, err := os.Open("./inp/footer.yaml")
+	yamlFil, err := os.Open("./inp/footer.yaml")
 	if err != nil {
 		fmt.Printf("error opening footer yaml file: %v\n", err)
 		os.Exit(-1)
 	}
-	defer infil.Close()
+	defer yamlFil.Close()
 
 	outfil, err := os.Create("./output/footer.html")
 	if err != nil {
@@ -50,7 +88,12 @@ func main () {
 	}
 	defer outfil.Close()
 
+	yamlObj, err := readYamlFil(yamlFil)
+	if err != nil {
+		fmt.Printf("error reading footer file: %v\n", err)
+		os.Exit(-1)
+	}
+	dispFooterYamlObj(yamlObj)
+
 	fmt.Println("*** success create footer ***")
 }
-
-
